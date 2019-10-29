311a02affd7b0ae3d779b2fa6467fed74d1d6f97
hyeokjin-eun <gurwls2399@gmail.com>
2019-10-29 22:55:24 +0900

    Lombok, postMethod, getMethod 생성 및 학습

---
+++ src/main/java/com/fast/admin/Controller/PostController.java
@@ -0,0 +1,17 @@
+package com.fast.admin.Controller;
+
+import com.fast.admin.model.SearchParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+    @PostMapping("/postMethod")
+    public SearchParam postMethod(@RequestBody SearchParam searchParam) {
+        return searchParam;
+    }
+}
---
+++ src/main/java/com/fast/admin/model/SearchParam.java
@@ -0,0 +1,12 @@
+package com.fast.admin.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SearchParam {
+    private String account;
+    private String email;
+    private int page;
+}
