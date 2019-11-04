cfea538c967ab60cf50aca49175303f8ab6d60ee
hyeokjin-eun <gurwls2399@gmail.com>
2019-11-04 19:17:23 +0900

    개발 환경 세팅 (schema, data) 파일 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,48 @@
+
+-- User 데이터 입력
+INSERT INTO `user` VALUES
+('Test1', 'Test1@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test2', 'Test2@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test3', 'Test3@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test4', 'Test4@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test5', 'Test5@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test6', 'Test6@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test7', 'Test7@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test8', 'Test8@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test9', 'Test9@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test10', 'Test10@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test11', 'Test11@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test12', 'Test12@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test13', 'Test13@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test14', 'Test14@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test15', 'Test15@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test16', 'Test16@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test17', 'Test17@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test18', 'Test18@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test19', 'Test19@test.com', '010-1234-1234', NOW(), 'ADMIN'),
+('Test20', 'Test20@test.com', '010-1234-1234', NOW(), 'ADMIN')
+
+-- Item 데이터 입력
+INSERT INTO `item` VALUES
+('노트북1', 100000, '노트북1 내용'),
+('노트북2', 200000, '노트북2 내용'),
+('노트북3', 300000, '노트북3 내용'),
+('노트북4', 400000, '노트북4 내용'),
+('노트북5', 500000, '노트북5 내용'),
+('노트북6', 600000, '노트북6 내용'),
+('노트북7', 700000, '노트북7 내용'),
+('노트북8', 800000, '노트북8 내용'),
+('노트북9', 900000, '노트북9 내용'),
+('노트북10', 1000000, '노트북10 내용')
+
+-- Order_Detail 데이터 입력
+INSERT INTO `order_detail` VALUES
+(NOW(), 1, 1),
+(NOW(), 2, 2),
+(NOW(), 3, 3),
+(NOW(), 4, 4),
+(NOW(), 5, 5),
+(NOW(), 1, 2),
+(NOW(), 1, 3),
+(NOW(), 1, 4),
+(NOW(), 1, 5)
