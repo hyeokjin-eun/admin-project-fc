93634fb6de8ce8d53d1524f07168463fea24a6b9
hyeokjin-eun <gurwls2399@gmail.com>
2019-12-16 00:10:07 +0900

    UserAPI response 및  User create API 개발

--- src/main/java/com/fast/admin/controller/api/UserApiController.java
+++ src/main/java/com/fast/admin/controller/api/UserApiController.java
@@ -2,27 +2,37 @@ package com.fast.admin.controller.api;
 
 import com.fast.admin.inter.CrudInterface;
 import com.fast.admin.model.network.Header;
+import com.fast.admin.model.network.request.UserApiRequest;
+import com.fast.admin.model.network.response.UserApiResponse;
+import com.fast.admin.service.UserApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface {
+public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    @Autowired
+    private UserApiLogicService userApiLogicService;
 
     @Override
     @PostMapping("")
-    public Header create() {
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        log.info("Request : {}", request);
+        return userApiLogicService.create(request);
     }
 
     @Override
     @GetMapping("{id}")
-    public Header read(@PathVariable("id") Long id) {
+    public Header<UserApiResponse> read(@PathVariable("id") Long id) {
         return null;
     }
 
     @Override
     @PutMapping("")
-    public Header update() {
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
         return null;
     }
 
